0126a8d9261144033a3262652eeebe713dc6d792
th-sakib <mdthamidulhasansakib@gmail.com>
2025-08-09 01:23:24 +0600

    Add after direcotry - which enable us to write configuration after all plugins are loaded Set up lualien (statusbar)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,13 +3,14 @@ require('config.lazy')
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", "<cmd>.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
+vim.opt.shiftwidth = 2
 
 -- Highlight when yanking text
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', {clear = true}),
-	callback = function ()
-		vim.highlight.on_yank()
-	end
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', {clear = true}),
+  callback = function ()
+    vim.highlight.on_yank()
+  end
 })
 
