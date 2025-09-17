16ca68208dc75f5989f344152609112d12d41392
th-sakib <mdthamidulhasansakib@gmail.com>
2025-09-17 21:22:25 +0600

    Add nvim - change colorscheme

--- nvim/.config/nvim/lua/config/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/config/plugins/colorscheme.lua
@@ -1,12 +1,23 @@
 return {
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  --   config = function()
+  --     -- vim.cmd.colorscheme("tokyonight-storm")
+  --     vim.cmd.colorscheme("tokyonight")
+  --   end,
+  -- },
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function ()
-      -- vim.cmd.colorscheme("tokyonight-storm")
-      vim.cmd.colorscheme("tokyonight")
-    end
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("onedark").setup({
+        style = "deep", -- 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+      })
+      -- Enable theme
+      require("onedark").load()
+    end,
   },
 }
