d28b8247ef9a955ab98ef25f4da81bbd53028dfd
th-sakib <mdthamidulhasansakib@gmail.com>
2025-10-03 23:31:32 +0600

    sway sleep & shutdown never worked

--- nvim/.config/nvim/lua/config/plugins/typescript-tools.lua
+++ nvim/.config/nvim/lua/config/plugins/typescript-tools.lua
@@ -1,5 +1,12 @@
 return {
   "pmizio/typescript-tools.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {},
+  opts = {
+    -- This setting is passed to nvim-lspconfig
+    root_dir = require("lspconfig.util").root_pattern(".git"),
+
+    settings = {
+      separate_diagnostic_server = false,
+    },
+  },
 }
