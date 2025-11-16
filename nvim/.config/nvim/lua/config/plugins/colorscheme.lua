3e0d349d3e44b096917e39470ca753f9e6c23135
th-sakib <mdthamidulhasansakib@gmail.com>
2025-11-16 19:35:17 +0600

    Add typescript formatter

--- nvim/.config/nvim/lua/config/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/config/plugins/colorscheme.lua
@@ -5,8 +5,8 @@ return {
     priority = 1000,
     opts = {},
     config = function()
-      -- vim.cmd.colorscheme("tokyonight-storm")
-      vim.cmd.colorscheme("tokyonight")
+      vim.cmd.colorscheme("tokyonight-storm")
+      -- vim.cmd.colorscheme("tokyonight")
     end,
   },
   -- {
--- nvim/.config/nvim/lua/config/plugins/conform.lua
+++ nvim/.config/nvim/lua/config/plugins/conform.lua
@@ -9,6 +9,7 @@ return {
       rust = { "rustfmt", lsp_format = "fallback" },
       -- Conform will run the first available formatter
       javascript = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
       html = { "prettierd", "prettier", stop_after_first = true },
     },
     -- format_on_save = {
